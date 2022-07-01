cde77b405d7ea1b6a5ab34cec6460f28bfa1fe4a
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-07-01 19:49:21 +0300

    data transfer accomplised to productDetails Page

--- src/Data.js
+++ src/Data.js
@@ -4,3 +4,8 @@ export const getProductList = async () => {
   const { data } =await axios.get("http://localhost:4000/product");
   return data;
 };
+
+export const getProduct = async (id) => {
+  const { data } =await axios.get(`http://localhost:4000/product/${id}`);
+  return data;
+};
--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../../Data";
 
 function ProductDetails() {
-  return (
-    <div>ProductDetails</div>
-  )
+  const { product_id } = useParams();
+  const { isLoading, isError, data } = useQuery(["product", product_id], () =>
+    getProduct(product_id)
+  );
+  if (isLoading) {
+    return <div> Loading... </div>;
+  }
+  if (isError) {
+    return <div>Error.</div>;
+  }
+  console.log(data);
+  return <div>
+    {data.title}
+    <img src={data.photos[0]} alt="" />
+    </div>;
 }
 
-export default ProductDetails
+export default ProductDetails;
