b533865f9264464b066f7432b71191290403f8b0
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-07-08 00:21:40 +0300

    login process completed

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,25 @@
 import axios from "axios";
 
+axios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    const { origin } = new URL(config.url);
+
+    const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+    const accessToken = localStorage.getItem("access-token");
+
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = accessToken;
+    }
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
 export const getProductList = async ({ pageParam = 0 }) => {
   const { data } = await axios.get(
     `${process.env.REACT_APP_BASE_ENDPOINT}/product?page=${pageParam}`
@@ -18,6 +38,13 @@ export const postRegister = async (input) => {
   const { data } = await axios.post(
     `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
     input
+  );
+  return data;
+};
+
+export const fetchMe = async () => {
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`
   );
   return data;
 };
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,24 +1,47 @@
-import {useState, createContext, useEffect, useContext} from "react";
+import { useState, createContext, useEffect, useContext } from "react";
+import { fetchMe } from "../Data";
 
-const AuthContext= createContext();
+const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState();
-    const [isLogin, setIsLogin] = useState(false);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [isLogin, setIsLogin] = useState(false);
 
-    const login = (data) => {
+  useEffect(() => {
+    // çağırdığımız fonksiyon async bir fonksiyon. Bu yüzden useEffect içinde bunu async olarak çağırmamız gerekli. bunu da aşağıdaki gibi yapılamadığı için içeride bir anonymus fonksiyon yazıldı.
+    (async () => {
+      try {
+        const me = await fetchMe();
+        console.log("me", me);
         setIsLogin(true);
-        setUser(data.user)
-    };
+        setUser(me);
 
-    const values = {
-        isLogin,
-        user,
-        login,
-    }
+      } catch (e) {}
+    })();
+  }, []);
 
-    return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+  // useEffect(async() => {
+  //   const me =await fetchMe();
+  //   console.log("me", me);
+  // }, [])
+
+  const login = (data) => {
+    setIsLogin(true);
+    setUser(data.user);
+
+    // datadan gelen access-token ve refresh-token'ı localstorage'a alıyoruz.
+    localStorage.setItem("access-token", data.accessToken);
+    localStorage.setItem("refresh-token", data.refreshToken);
+  };
+
+  const values = {
+    isLogin,
+    user,
+    login,
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
 const UseAuth = () => useContext(AuthContext);
-export {AuthProvider, UseAuth}
+export { AuthProvider, UseAuth };
