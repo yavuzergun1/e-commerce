c180963a92a7077efc6685823bac4c9b4e077153
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-07-12 21:32:28 +0300

    localstorage settet to basket

--- src/contexts/BasketContext.js
+++ src/contexts/BasketContext.js
@@ -1,29 +1,39 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { createContext } from "react";
 import { UseAuth } from "./AuthContext";
 
 const BasketContext = createContext();
+const defaultBasket = JSON.parse(localStorage.getItem("basket")) || [];
 
 const BasketProvider = ({ children }) => {
-  const{isLogin} = UseAuth();
-  const [items, setItems] = useState([]);
+  const { isLogin } = UseAuth();
+  const [items, setItems] = useState(defaultBasket);
+
+  useEffect(() => {
+    localStorage.setItem("basket", JSON.stringify(items));
+  }, [items]);
 
   const addToBasket = (data, isBasketItem, item) => {
     console.log("basket items", items);
-// Eğer ürün sepetteyse sepetten çıkar
+    // Eğer ürün sepetteyse sepetten çıkar
     if (isBasketItem) {
       const filtered = items.filter((item) => item._id !== isBasketItem._id);
       return setItems(filtered);
-    } if(isLogin){
-      setItems((prev) => [...prev, data]);/* ürün sepette değilse ve üye girişi yapılmışsa sepete ekle */
-    } else {alert("please login or sign up")} /* üye girişi yapılmamışsa uyarı ver */
-    
+    }
+    if (isLogin) {
+      setItems((prev) => [
+        ...prev,
+        data,
+      ]); /* ürün sepette değilse ve üye girişi yapılmışsa sepete ekle */
+    } else {
+      alert("please login or sign up");
+    } /* üye girişi yapılmamışsa uyarı ver */
   };
 
   const values = {
     items,
     setItems,
-    addToBasket
+    addToBasket,
   };
   return (
     <BasketContext.Provider value={values}>{children}</BasketContext.Provider>
--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -1,23 +1,36 @@
-import { Alert, Box, Grid } from "@chakra-ui/react";
+import { Alert, Box, Flex, Grid } from "@chakra-ui/react";
 import React from "react";
 import Card from "../../components/Card/Card";
 import { UseBasket } from "../../contexts/BasketContext";
 import "./basket.scss";
 function Basket() {
   const { items } = UseBasket();
+  const total = items.reduce((acc, curr) => acc + curr.price, 0);
+
+  
+  
 
   return (
     <div>
       {items.length < 1 && <Alert status="warning">Box is Empty</Alert>}
-      <Grid templateColumns="repeat(3, 2fr)" gap={6} px="5" py="2" > 
-
+      {/* <Grid templateColumns="repeat(3, 2fr)" gap={6} px="5" py="2" autoFlow="row dense" >  */}
+      <div className="main-container">
+        <div className="cards-container">
           {items.map((item) => (
             <React.Fragment key={item._id}>
+              <div className="card-container">
                 <Card item={item} />
+              </div>
             </React.Fragment>
           ))}
-          </Grid>
+          {/* </Grid> */}
+
         </div>
+          <Flex m="10" justifyContent="center" fontSize="25">
+            {total > 0 && `Total= ${total}TL`}
+          </Flex>
+      </div>
+    </div>
   );
 }
 
