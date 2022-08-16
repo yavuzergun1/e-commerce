b4fcd729be7a372454666222c5ecd5411384dd80
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-08-17 00:49:08 +0300

    orderdetails page

--- src/pages/OrderDetails/OrderDetails.js
+++ src/pages/OrderDetails/OrderDetails.js
@@ -10,22 +10,6 @@ import GetProductIdByUseQuery from "../../components/getProductIdByUseQuery/GetP
 function OrderDetails() {
   const { response } = UseBasket();
   console.log("response", response);
-  // response.items.map((item) => console.log("item id", item));
-
-  // aşağıdaki sorun useQuery kullanmadan bu şekilde çözülebilir ama devam edersem hem burada hem de data componentinde ekstra kod satırına sebep olur.
-
-  // useEffect(() => {
-  //  response.items.map((item) => getProduct(item))
-  // }, [])
-  
-
-  // Spariş verildikten sonra useNavigate ile orderDetails sayfasına yönlendiriyorum. Burada response'dan gelen product id'lerini data.js içerisinde bulunan getProduct'a gönderip fetchleyerek bu id'deki ürünlerin detay bilgilerine ulaşmak istiyorum. id'ler getProduct'a ulaşıyor. ama fetchleme sonucu undefined geliyor. map metodunu useQuery içinde nasıl çözebilirim?
-
-  // <getProductIdByUseQuery/>
-  
-
- 
- 
 
   return (
     <div>
