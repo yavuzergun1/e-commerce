8ffb6204d7047a3b896c1fd1277cedaa131e80cc
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-08-17 00:48:20 +0300

    useQuery fetching problem solved!

--- src/pages/OrderDetails/OrderDetails.js
+++ src/pages/OrderDetails/OrderDetails.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import { UseBasket } from '../../contexts/BasketContext';
-import { useQuery } from 'react-query';
-import { Flex,Spinner} from "@chakra-ui/react";
-import { getProduct } from '../../Data';
+import React from "react";
+import { UseBasket } from "../../contexts/BasketContext";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { Flex, Spinner } from "@chakra-ui/react";
+import { getProduct } from "../../Data";
+import GetProductIdByUseQuery from "../../components/getProductIdByUseQuery/GetProductIdByUseQuery";
+
 
 function OrderDetails() {
-  const {response} = UseBasket();
+  const { response } = UseBasket();
   console.log("response", response);
-response.items.map(item => console.log("item id",item))
+  // response.items.map((item) => console.log("item id", item));
 
-//Spariş verildikten sonra useNavigate ile orderDetails sayfasına yönlendiriyorum. Burada response'dan gelen product id'lerini data.js içerisinde bulunan getProduct'a gönderip fetchleyerek bu id'deki ürünlerin detay bilgilerine ulaşmak istiyorum. id'ler getProduct'a ulaşıyor. ama fetchleme sonucu undefined geliyor.
-  const { isLoading, isError, data } = useQuery("orderId", response.items.map((item) =>
- getProduct(item))
-  );
-  console.log("data",data);
-  if (isLoading) {
-    return (
-      <Flex justifyContent="center" alignItems="center" height="100vh">
-      <Spinner size="xl"/>
-    </Flex>
-  );
-}
-if (isError) {
-  return <div>Error.</div>;
-}
-return (
-  <div>
+  // aşağıdaki sorun useQuery kullanmadan bu şekilde çözülebilir ama devam edersem hem burada hem de data componentinde ekstra kod satırına sebep olur.
 
-    
-    <div>Your Order Details</div>
-    <div>Adress 
-      <br/>
-      {response.address} 
+  // useEffect(() => {
+  //  response.items.map((item) => getProduct(item))
+  // }, [])
+  
+
+  // Spariş verildikten sonra useNavigate ile orderDetails sayfasına yönlendiriyorum. Burada response'dan gelen product id'lerini data.js içerisinde bulunan getProduct'a gönderip fetchleyerek bu id'deki ürünlerin detay bilgilerine ulaşmak istiyorum. id'ler getProduct'a ulaşıyor. ama fetchleme sonucu undefined geliyor. map metodunu useQuery içinde nasıl çözebilirim?
+
+  // <getProductIdByUseQuery/>
+  
+
+ 
+ 
+
+  return (
+    <div>
+      <div>Your Order Details</div>
+      <div>
+        Adress
+        <br />
+        {response.address}
       </div>
-      </div>
-  )
+     { response.items.map((item) => <GetProductIdByUseQuery item={item} /> )}
+    </div>
+  );
 }
 
-export default OrderDetails
+export default OrderDetails;
