d6d1c0f440abeb858d9cfc6b6fce2eb3544d7380
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-08-19 22:43:50 +0300

    admin/product page building

--- src/Data.js
+++ src/Data.js
@@ -80,3 +80,8 @@ export const getOrders = async () => {
   const { data } = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/order`);
   return data;
 };
+
+export const deleteProduct = async (product_id) =>{
+  const {data} = await axios.delete(`${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`);
+  return data;
+}
--- src/pages/Admin/products/Products.js
+++ src/pages/Admin/products/Products.js
@@ -1,54 +1,87 @@
-import React from "react";
-import { useQuery } from "react-query";
-import { getProductList } from "../../../Data";
-import { Table } from "antd";
+import { useMemo } from "react";
+import { useQuery, useMutation } from "react-query";
+import { useState } from "react";
+import { getProductList, deleteProduct } from "../../../Data";
+import { Table, Popconfirm, message } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 import { Text } from "@chakra-ui/react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Products() {
+  const [visible, setVisible] = useState(false);
   const { isLoading, isError, data, error } = useQuery(
     "admin:products",
     getProductList
   );
+  /* Silme işlemi mutation'a giriyor. bu sebeple useQuery yerine burada useMutation kullanılıyor */
+  const deleteMutation = useMutation(deleteProduct);
 
-  const columns= [{
-    title: 'Title',
-    dataIndex: 'title',
-    key: 'title'
-  },{
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price'
-  },
-{
-  title: 'createdAt',
-  dataIndex: 'createdAt',
-  key: 'createdAt'
-},
-{
-  title: 'Action',
-  dataIndex: 'action',
-  /* buton koyulacak yere render edilecek fonksiyon yazıldı */
-  render: (text, record) => (
-    <>
-    <Link to={`/admin/products/${record._id}`} >Edit</Link>
-    </>
-  )
-}]
+  /* Bu component her render edildiğinde columns yeniden hesaplanmaya çalışılacak. Bunu engellemek için useMemo kullanıldı. */
+  const columns = useMemo(() => {
+    return [
+      {
+        title: "Title",
+        dataIndex: "title",
+        key: "title",
+      },
+      {
+        title: "Price",
+        dataIndex: "price",
+        key: "price",
+      },
+      {
+        title: "createdAt",
+        dataIndex: "createdAt",
+        key: "createdAt",
+      },
+      {
+        title: "Action",
+        dataIndex: "action",
+        /* buton koyulacak yere render edilecek fonksiyon yazıldı */
+        render: (text, record) => (
+          <>
+            <Link to={`/admin/products/${record._id}`}>Edit</Link>
+            <Popconfirm
+              title="Are you sure?"
+              okText="Yes"
+              cancelText="No"
+              onCancel={() => console.log("cancel")}
+              onConfirm={() => {
+                deleteMutation.mutate(
+                  record._id
+                ); /* ilgili satırdaki id'ye ait product'u siler */
+                setVisible(false); /* confirm olduğunda onay bölümünü kapatır */
+                message.success(
+                  "Product Succesfully Deleted"
+                ); /* confirm olduğunda yandaki mesajı gösterir */
+              }}
+            >
+              <a style={{ marginLeft: 10, color: "tomato" }} href="#">
+                Delete
+              </a>
+            </Popconfirm>
+          </>
+        ),
+      },
+    ];
+  }, []);
 
-  if (isLoading){
-    return <div>Loading</div>
+  if (isLoading) {
+    return <div>Loading</div>;
   }
   if (isError) {
     return <div>Error {error.message}</div>;
   }
   console.log(data);
 
-  return <div>
-  <Text fontSize={20} p="5">Products</Text>
-
-  <Table dataSource={data} columns={columns} rowKey="_id" />;
-  </div>;
+  return (
+    <div>
+      <Text fontSize={20} p="5">
+        Products
+      </Text>
+      <Table dataSource={data} columns={columns} rowKey="_id" />;
+    </div>
+  );
 }
 
 export default Products;
